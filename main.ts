3b092bed09054e94a86c32d3d58bb46ecbe5d141
BasedLmao <117203633+BasedBurnerOngFrFr@users.noreply.github.com>
2022-11-03 10:57:23 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,20 @@
-basic.forever(function () {
-    if (true) {
-        let dangerous_co2_level = 0
-        if (gatorEnvironment.getMeasurement(measurementType.eCO2) >= dangerous_co2_level) {
-            basic.showString("warning")
-        }
-    } else {
-        led.plotBarGraph(
-        gatorEnvironment.getMeasurement(measurementType.eCO2),
-        20
-        )
+let dangerous_co2_level = 0
+function funtion_two () {
+	
+}
+function function_one () {
+    if (gatorEnvironment.getMeasurement(measurementType.eCO2) >= dangerous_co2_level) {
+        basic.showString("warning")
     }
+    led.plotBarGraph(
+    gatorEnvironment.getMeasurement(measurementType.eCO2),
+    20
+    )
+}
+basic.forever(function () {
+    function_one()
+    funtion_two()
+    dangerous_co2_level = 20
 })
 basic.forever(function () {
 	
